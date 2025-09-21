2c09be36a272bb0e4b6feec5d7fb0e24e5750962
und1n3 <undineunda5@gmail.com>
2025-09-21 22:21:07 +0200

    test

--- src/nabu_agent/workflows/main/workflow.py
+++ src/nabu_agent/workflows/main/workflow.py
@@ -16,31 +16,43 @@ def decide_action(state: MainGraphState) -> QuestionType:
     return END
 
 
+import requests
+
+
+def test_function(state: MainGraphState):
+    requests.get(url="https://www.google.com")
+    return state
+
+
 def build_main_workflow() -> CompiledStateGraph:
     workflow = StateGraph(MainGraphState)
-    workflow.add_node("Translator", nodes.translate_to_english)
-    workflow.add_node("Enrouting Question", nodes.enroute_question)
-    workflow.add_node("Pre-stablished commands", nodes.pre_established_commands)
-    workflow.add_node("Internet search", nodes.internet_search)
-    workflow.add_node("Spotify Command", build_spotify_workflow())
-    workflow.add_node("Finish Action", nodes.finish_action)
+    workflow.add_node("test", test_function)
+    workflow.set_entry_point("test")
+    workflow.set_finish_point("test")
 
-    workflow.set_entry_point("Translator")
-    workflow.add_edge("Translator", "Enrouting Question")
-    workflow.add_conditional_edges(
-        "Enrouting Question",
-        decide_action,
-        {
-            QuestionType.internet.value: "Internet search",
-            QuestionType.party.value: "Pre-stablished commands",
-            QuestionType.spotify.value: "Spotify Command",
-            END: END,
-        },
-    )
-    workflow.add_edge("Pre-stablished commands", "Finish Action")
-    workflow.add_edge("Internet search", "Finish Action")
-    workflow.add_edge("Spotify Command", "Finish Action")
-    workflow.set_finish_point("Finish Action")
+    # workflow.add_node("Translator", nodes.translate_to_english)
+    # workflow.add_node("Enrouting Question", nodes.enroute_question)
+    # workflow.add_node("Pre-stablished commands", nodes.pre_established_commands)
+    # workflow.add_node("Internet search", nodes.internet_search)
+    # workflow.add_node("Spotify Command", build_spotify_workflow())
+    # workflow.add_node("Finish Action", nodes.finish_action)
+
+    # workflow.set_entry_point("Translator")
+    # workflow.add_edge("Translator", "Enrouting Question")
+    # workflow.add_conditional_edges(
+    #     "Enrouting Question",
+    #     decide_action,
+    #     {
+    #         QuestionType.internet.value: "Internet search",
+    #         QuestionType.party.value: "Pre-stablished commands",
+    #         QuestionType.spotify.value: "Spotify Command",
+    #         END: END,
+    #     },
+    # )
+    # workflow.add_edge("Pre-stablished commands", "Finish Action")
+    # workflow.add_edge("Internet search", "Finish Action")
+    # workflow.add_edge("Spotify Command", "Finish Action")
+    # workflow.set_finish_point("Finish Action")
     return workflow.compile()
 
 
